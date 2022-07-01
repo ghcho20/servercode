853c2501a5e61643fd7c338d95cd031abb248787
sendnerd <gunho.cho@sendbird.com>
2022-07-02 00:03:05 +0900

    site frame setup (header + side menu)

---
+++ components/layout.js
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function Layout({ children }) {
+  const router = useRouter();
+
+  const menuItems = [
+    {
+      href: '/chat',
+      title: 'Dashboard',
+    },
+    {
+      href: '/chat/users',
+      title: 'Users',
+    },
+    {
+      href: '/chat/channels',
+      title: 'Channels',
+    },
+  ];
+
+  return (
+    <div className='min-h-screen flex flex-col text-white'>
+      <header className='bg-emerald-600 sticky top-0 h-14 flex justify-center items-center font-semibold uppercase'>
+        flybird.com dashboard
+      </header>
+      <div className='flex flex-col md:flex-row flex-1'>
+        <aside className='bg-emerald-600 w-full md:w-60'>
+          <nav>
+            <ul>
+              {menuItems.map(({ href, title }) => (
+                <li className='m-2' key={title}>
+                  <Link href={href}>
+                    <a
+                      className={`flex p-2 bg-emerald-600 rounded hover:bg-emerald-300 cursor-pointer ${
+                        router.asPath === href && 'bg-emerald-700'
+                      }`}
+                    >
+                      {title}
+                    </a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </aside>
+        <main className='flex-1'>{children}</main>
+      </div>
+    </div>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css';
+import Head from "next/head";
 import { useRouter } from 'next/router'
 
-import Head from "next/head";
+import '../styles/globals.css';
+import Layout from '../components/layout';
+
 import Container from "../components/Container";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
@@ -9,7 +11,13 @@ import Header from "../components/Header";
 function MyApp({ Component, pageProps }) {
     const router = useRouter()
     if (router.pathname.startsWith('/chat')) {
-        <Component {...pageProps} />
+        return(
+            <>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+            </>
+        )
     } else {
         return (
             <div className="flex flex-col items-center justify-center min-h-screen py-2">
