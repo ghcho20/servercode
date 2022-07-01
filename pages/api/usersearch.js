6ee47c972d3b167c810cc022b4edd8248d83773e
sendnerd <gunho.cho@sendbird.com>
2022-07-02 07:55:02 +0900

    search users - fuzzy enabled

--- pages/api/usersearch.js
+++ pages/api/usersearch.js
@@ -24,7 +24,8 @@ export default async function handler( req, res) {
                 query: q,
                 path: {
                     wildcard: '*'
-                }
+                },
+                fuzzy: {}
             }
         }
     }]
